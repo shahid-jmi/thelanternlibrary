4ac200462c26490e5d280a8e4140139008c6fdaa
shahid-jmi <shahidsalafi4@gmail.com>
2026-09-17 13:23:31 +0530

    fix: update Instagram link in ContactSection component

--- frontend/src/app/pages/home/ContactSection.tsx
+++ frontend/src/app/pages/home/ContactSection.tsx
@@ -22,7 +22,7 @@ export default function ContactSection() {
             color="green"
           />
           <ContactLink
-            href="https://instagram.com/lanternlibrary"
+            href="https://instagram.com/thelanternlibraryy"
             icon={<Instagram className="h-5 w-5" />}
             title="Instagram"
             detail="@thelanternlibraryy"
